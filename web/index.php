8771499fd389318956acfc85e5d44aaa8433f033
Rbn3D <ruben3d3@gmail.com>
2014-08-22 15:00:44 +0200

    Implemented twig view system. Basic view to test disqus Audience Sync

--- web/index.php
+++ web/index.php
@@ -4,20 +4,101 @@ require_once __DIR__.'/../vendor/autoload.php';
 
 $app = new Silex\Application();
 $app->register(new DerAlex\Silex\YamlConfigServiceProvider(__DIR__ . '/../settings.yml'));
+$app->register(new Silex\Provider\TwigServiceProvider(), array('twig.path' => __DIR__. '/../views'));
 
+$app['debug'] = true;
 
-//$app['debug'] = true;
 
 // Disqus controllers, should be moved to another file as described here (http://silex.sensiolabs.org/doc/providers.html#controller-providers) if gets too fat
 $disqus = $app['controllers_factory'];
 
 $disqus->get('/as_callback', function (Symfony\Component\HttpFoundation\Request $request) use ($app)
 {
-	var_dump($app["config"]);
+	if(isset($request->attributes->parameters['verify']))
+	{
+		// Return terms of service
+
+	}
+	else
+	{
+		// Get the access token
+
+		// Get the code for request access
+	    $code = $request->attributes->parameters['code'];
+	    $audiencesync_uri = urldecode($request->attributes->parameters['audiencesync_uri']);
+
+	    // Request the access token
+	    extract($_POST);
+
+	    $url = 'https://disqus.com/api/oauth/2.0/access_token/';
+	    $fields = array(
+		    'grant_type'=>urlencode("audiencesync"),
+		    'client_id'=>urlencode(constant("DisqusApiPublic")),
+		    'client_secret'=>urlencode(constant("DisqusApiSecret")),
+		    'redirect_uri'=>urlencode(constant("BaseSitePath") . 'as-callback.php?verify=1'),
+		    'code'=>urlencode($code)
+	    );
+
+	    //url-ify the data for the POST
+	    foreach($fields as $key=>$value) { $fields_string .= $key.'='.$value.'&'; }
+	    rtrim($fields_string, "&");
+
+	    //open connection
+	    $ch = curl_init();
+
+	    //set the url, number of POST vars, POST data
+	    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	    curl_setopt($ch,CURLOPT_URL,$url);
+	    curl_setopt($ch,CURLOPT_POST,count($fields));
+	    curl_setopt($ch,CURLOPT_POSTFIELDS,$fields_string);
+
+	    //execute post
+	    $result = curl_exec($ch);
+
+	    //close connection
+	    curl_close($ch);
+
+	    $auth_results = json_decode($result);
+
+	    if (isset($auth_results->error)) {
+
+	        die($auth_results->error);
+	    
+	    }
+
+	    // Extract access token and render
+	    $access_token = $auth_results->access_token;
+	    $user_id = $auth_results->user_id;
+	    $success = $request->attributes->parameters['success'];
+
+	    var_dump($auth_results);
+
+	    $completion_url = $audiencesync_uri . "?client_id=" . constant("DisqusApiPublic") . "&user_id=" . $user_id . "&access_token=" . $access_token . "&success=" . $success;
+
+	    /*echo   '<script type="text/javascript">
+	            window.location = "' . $completion_url . '";
+	            </script>';*/
+	}
+
 	var_dump($request);
 	return 'df';
 });
 
+// Debug controllers (local site to test disqus stuf)
+$debug = $app['controllers_factory'];
+
+$debug->get("/", function() use ($app)
+{
+	// Render a simple page with disqus comments
+	$parameters = array('config'=>$app['config']);
+
+    return $app['twig']->render('disqus_debug_page.twig', $parameters);
+});
+
+
 $app->mount("/disqus", $disqus);
+
+if($app['debug'])
+	$app->mount("/debug", $debug);
 
 $app->run();
