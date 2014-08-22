2ffb302881f8805a8d71d3190872cb09484195a6
Rbn3D <ruben3d3@gmail.com>
2014-08-22 13:35:48 +0200

    First commit. Initial project set up and yaml configuration provider

--- web/index.php
+++ web/index.php
@@ -3,9 +3,21 @@
 require_once __DIR__.'/../vendor/autoload.php';
 
 $app = new Silex\Application();
+$app->register(new DerAlex\Silex\YamlConfigServiceProvider(__DIR__ . '/../settings.yml'));
 
-$app->get('/hello', function() {
-    return 'Hello!';
+
+//$app['debug'] = true;
+
+// Disqus controllers, should be moved to another file as described here (http://silex.sensiolabs.org/doc/providers.html#controller-providers) if gets too fat
+$disqus = $app['controllers_factory'];
+
+$disqus->get('/as_callback', function (Symfony\Component\HttpFoundation\Request $request) use ($app)
+{
+	var_dump($app["config"]);
+	var_dump($request);
+	return 'df';
 });
+
+$app->mount("/disqus", $disqus);
 
 $app->run();
