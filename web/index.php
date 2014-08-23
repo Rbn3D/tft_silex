b1f346ff2e0e15c157bbbbc0fb0414d34b6f5853
Rbn3D <ruben3d3@gmail.com>
2014-08-23 18:51:52 +0200

    Refactoring

--- web/index.php
+++ web/index.php
@@ -7,114 +7,7 @@ $app->register(new Silex\Provider\UrlGeneratorServiceProvider());
 $app->register(new DerAlex\Silex\YamlConfigServiceProvider(__DIR__ . '/../settings.yml'));
 $app->register(new Silex\Provider\TwigServiceProvider(), array('twig.path' => __DIR__. '/../views'));
 
-
 $app['debug'] = true;
-
-
-// Disqus controllers, should be moved to another file as described here (http://silex.sensiolabs.org/doc/providers.html#controller-providers) if gets too fat
-$disqus = $app['controllers_factory'];
-
-$disqus->get('/as_callback', function (Symfony\Component\HttpFoundation\Request $request) use ($app)
-{
-	if($request->query->get('verify', false))
-	{
-		// Return terms of service
-
-	}
-	else
-	{
-		// Get the access token
-
-		// Get the code for request access
-	    $code = $request->query->get('code');
-	    $audiencesync_uri = urldecode($request->query->get('audiencesync_uri'));
-
-	    // Request the access token
-	    extract($_POST);
-
-	    $url = 'https://disqus.com/api/oauth/2.0/access_token/';
-	    $fields = array(
-		    'grant_type'=>urlencode("audiencesync"),
-		    'client_id'=>urlencode($app["config"]["disqus.audsync.publickey"]),
-		    'client_secret'=>urlencode($app["config"]["disqus.audsync.secret"]),
-		    'redirect_uri'=>urlencode($app->url('disqus_as_callback')),
-		    'code'=>urlencode($code)
-	    );
-
-	    $fields_string = "";
-	    //url-ify the data for the POST
-	    foreach($fields as $key=>$value) { $fields_string .= $key.'='.$value.'&'; }
-	    rtrim($fields_string, "&");
-
-	    //open connection
-	    $ch = curl_init();
-
-	    //set the url, number of POST vars, POST data
-	    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-	    curl_setopt($ch,CURLOPT_URL,$url);
-	    curl_setopt($ch,CURLOPT_POST,count($fields));
-	    curl_setopt($ch,CURLOPT_POSTFIELDS,$fields_string);
-
-	    //execute post
-	    $result = curl_exec($ch);
-
-	    //close connection
-	    curl_close($ch);
-
-	    $auth_results = json_decode($result);
-
-	    //var_dump($auth_results);
-
-	    if (isset($auth_results->error)) 
-	    {
-	        die($auth_results->error);
-	    }
-
-	    // Extract access token and render
-	    $access_token = $auth_results->access_token;
-	    $user_id = $auth_results->user_id;
-	    $success = $request->query->get('success');
-
-	    // If success, ask for user details
-	    $url = 'https://disqus.com/api/3.0/users/details.json';
-
-		$query = http_build_query(array(
-				'access_token' => $access_token,
-				'api_key' => $app["config"]["disqus.audsync.publickey"],
-				'api_secret' => $app["config"]["disqus.audsync.secret"]
-			));
-
-		$url .= '?'.$query;
-		var_dump($url);
-
-	    //open connection
-	    $ch = curl_init();
-
-	    //set the url, number of POST vars, POST data
-	    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-	    curl_setopt($ch,CURLOPT_URL,$url);
-
-	    //execute post
-	    $result = curl_exec($ch);
-
-	    //close connection
-	    curl_close($ch);
-
-	    $user_details = json_decode($result);
-
-	    var_dump($user_details);
-	    var_dump($user_details->response->email);
-
-	    $completion_url = "";
-
-	    /*echo   '<script type="text/javascript">
-	            window.location = "' . $completion_url . '";
-	            </script>';*/
-	}
-
-	//var_dump($request);
-	return '_';
-})->bind('disqus_as_callback');
 
 // Debug controllers (local site to test disqus stuff)
 $debug = $app['controllers_factory'];
@@ -128,7 +21,7 @@ $debug->get("/", function() use ($app)
 });
 
 
-$app->mount("/disqus", $disqus);
+$app->mount("/disqus", new TFT\Controllers\DisqusController());
 
 if($app['debug'])
 	$app->mount("/debug", $debug);
