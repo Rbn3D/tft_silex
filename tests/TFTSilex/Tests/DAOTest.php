aa67f42f2ef036e41ba4e978c600b83c59197c55
Rbn3D <ruben3d3@gmail.com>
2014-08-25 14:58:31 +0200

    Updated tests

--- tests/TFTSilex/Tests/DAOTest.php
+++ tests/TFTSilex/Tests/DAOTest.php
@@ -1,26 +1,28 @@
 <?php
+namespace TFTSilex\Tests;
 
-namespace YourApp\Tests;
+require_once __DIR__.'/../../../vendor/autoload.php';
 
 use Silex\WebTestCase;
 
-class YourTest extends WebTestCase
+class DAOTest extends WebTestCase
 {
-	private $app;
-
     public function createApplication()
     {
-        $app = require __DIR__.'/../../../../web/index.php';
+        $app = require __DIR__.'/../../../web/index.php';
 
         $app['debug'] = true;
 	    $app['exception_handler']->disable();
 
-	    $this->app = $app;
+	    //$this->app = $app;
 	    return $app;
     }
 
     public function testCreateUserDetails()
     {
-        $this->app['daomanager']->getUserDetailsDao()->save(new \DAO\Model\UserDetails('2572652', 'test@mail.com'));
+        $this->app['daomanager']->getUsetDetailsDAO()->save(new \TFT\Model\UserDetails('2572652', 'test@mail.com'));
+        $user = $this->app['daomanager']->getUsetDetailsDAO()->queryOneBy("disqus_user_id", '2572652');
+
+        $this->assertSame((string) '2572652', (string) $user->getDisqusUserId());
     }
 }
