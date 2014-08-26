15bdcef37835bb0bdbba289921032b5bdd8b0708
Rbn3D <ruben3d3@gmail.com>
2014-08-26 13:14:20 +0200

    Some refactorization, separated TOS rendering action from as_callback. as_callback now redirect user to uri endpoint correctly. There is still a problem when you try to see the terms of service and then go back to the audience sync authorization page. Maybe history.back() could help...

--- src/TFT/Controllers/DisqusController.php
+++ src/TFT/Controllers/DisqusController.php
@@ -16,106 +16,119 @@ class DisqusController implements ControllerProviderInterface
 
 		$disqus->get('/as_callback', function (\Symfony\Component\HttpFoundation\Request $request) use ($app)
 		{
-			if($request->query->get('verify', false))
-			{
-				// Return terms of service
-				echo 'TOS';
-			}
-			else
-			{
-				// Get the access token
+			// Get the access token
 
-				// Get the code for request access
-			    $code = $request->query->get('code');
-			    $audiencesync_uri = urldecode($request->query->get('audiencesync_uri'));
+			// Get the code for request access
+		    $code = $request->query->get('code');
+		    $audiencesync_uri = urldecode($request->query->get('audiencesync_uri'));
 
-			    // Request the access token
-			    extract($_POST);
+		    // Request the access token
+		    extract($_POST);
 
-			    $url = 'https://disqus.com/api/oauth/2.0/access_token/';
-			    $fields = array(
-				    'grant_type'=>urlencode("audiencesync"),
-				    'client_id'=>urlencode($app["config"]["disqus.audsync.publickey"]),
-				    'client_secret'=>urlencode($app["config"]["disqus.audsync.secret"]),
-				    'redirect_uri'=>urlencode($app->url('disqus_as_callback')),
-				    'code'=>urlencode($code)
-			    );
+		    $url = 'https://disqus.com/api/oauth/2.0/access_token/';
+		    $fields = array(
+			    'grant_type'=>urlencode("audiencesync"),
+			    'client_id'=>urlencode($app["config"]["disqus.audsync.publickey"]),
+			    'client_secret'=>urlencode($app["config"]["disqus.audsync.secret"]),
+			    'redirect_uri'=>urlencode($app->url('disqus_as_callback')),
+			    'code'=>urlencode($code)
+		    );
 
-			    $fields_string = "";
-			    //url-ify the data for the POST
-			    foreach($fields as $key=>$value) { $fields_string .= $key.'='.$value.'&'; }
-			    rtrim($fields_string, "&");
+		    $fields_string = "";
+		    //url-ify the data for the POST
+		    foreach($fields as $key=>$value) { $fields_string .= $key.'='.$value.'&'; }
+		    rtrim($fields_string, "&");
 
-			    //open connection
-			    $ch = \curl_init();
+		    //open connection
+		    $ch = \curl_init();
 
-			    //set the url, number of POST vars, POST data
-			    \curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-			    \curl_setopt($ch,CURLOPT_URL,$url);
-			    \curl_setopt($ch,CURLOPT_POST,count($fields));
-			    \curl_setopt($ch,CURLOPT_POSTFIELDS,$fields_string);
+		    //set the url, number of POST vars, POST data
+		    \curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		    \curl_setopt($ch,CURLOPT_URL,$url);
+		    \curl_setopt($ch,CURLOPT_POST,count($fields));
+		    \curl_setopt($ch,CURLOPT_POSTFIELDS,$fields_string);
 
-			    //execute post
-			    $result = \curl_exec($ch);
+		    //execute post
+		    $result = \curl_exec($ch);
 
-			    //close connection
-			    \curl_close($ch);
+		    //close connection
+		    \curl_close($ch);
 
-			    $auth_results = json_decode($result);
+		    $auth_results = json_decode($result);
 
-			    //var_dump($auth_results);
+		    //var_dump($auth_results);
 
-			    if (isset($auth_results->error)) 
-			    {
-			        die($auth_results->error);
-			    }
+		    if (isset($auth_results->error)) 
+		    {
+		        die($auth_results->error);
+		    }
 
-			    // Extract access token and render
-			    $access_token = $auth_results->access_token;
-			    $user_id = $auth_results->user_id;
-			    $success = $request->query->get('success');
+		    // Extract access token and render
+		    $access_token = $auth_results->access_token;
+		    $user_id = $auth_results->user_id;
+		    $success = $request->query->get('success');
 
-			    // If success, ask for user details
-			    $url = 'https://disqus.com/api/3.0/users/details.json';
+		    // If success, ask for user details
+		    $url = 'https://disqus.com/api/3.0/users/details.json';
 
-				$query = http_build_query(array(
-						'access_token' => $access_token,
-						'api_key' => $app["config"]["disqus.audsync.publickey"],
-						'api_secret' => $app["config"]["disqus.audsync.secret"]
-					));
+			$query = http_build_query(array(
+					'access_token' => $access_token,
+					'api_key' => $app["config"]["disqus.audsync.publickey"],
+					'api_secret' => $app["config"]["disqus.audsync.secret"]
+				));
 
-				$url .= '?'.$query;
+			$url .= '?'.$query;
 
-			    //open connection
-			    $ch = \curl_init();
+		    //open connection
+		    $ch = \curl_init();
 
-			    //set the url, number of POST vars, POST data
-			    \curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-			    \curl_setopt($ch,CURLOPT_URL,$url);
+		    //set the url, number of POST vars, POST data
+		    \curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		    \curl_setopt($ch,CURLOPT_URL,$url);
 
-			    //execute post
-			    $result = \curl_exec($ch);
+		    //execute post
+		    $result = \curl_exec($ch);
 
-			    //close connection
-			    \curl_close($ch);
+		    //close connection
+		    \curl_close($ch);
 
-			    $user_details = json_decode($result);
+		    $user_details = json_decode($result);
 
-			    $user = new \TFT\Model\UserDetails($user_id, $user_details->response->email);
+		    $user = new \TFT\Model\UserDetails($user_id, $user_details->response->email);
 
-			    $daoManager = $app['daomanager'];
-			    $daoManager->getUsetDetailsDAO()->save($user);
+		    $daoManager = $app['daomanager'];
+		    $daoManager->getUsetDetailsDAO()->save($user);
 
-			    $completion_url = "";
+		    // Finalize process
 
-			    /*echo   '<script type="text/javascript">
-			            window.location = "' . $completion_url . '";
-			            </script>';*/
-			}
+		    $end_url = $audiencesync_uri ."?". http_build_query(array(
+	    			'client_id'=>$app["config"]["disqus.audsync.publickey"],
+	    			'user_id'=>$user_id,
+	    			'access_token'=>$access_token,
+	    			'success'=>1
+	    		));
 
-			//var_dump($request);
-			return '_';
+			//Rendirect user to end point
+			return $app->redirect($end_url);
+
 		})->bind('disqus_as_callback');
+
+	    $disqus->get('/as_callback/tos', function (\Symfony\Component\HttpFoundation\Request $request) use ($app)
+	    {
+	    	//var_dump($request);
+	    	$user_id = $request->request->get('user_id');
+	    	$code = $request->request->get('user_id');
+	    	$audiencesync_uri = $request->request->get('audiencesync_uri');
+
+	    	$accept_url = $app->url('disqus_as_callback', array(
+	    			'user_id'=>$user_id,
+	    			'success'=>1,
+	    			'code'=>$code,
+	    			'audiencesync_uri'=>urlencode($audiencesync_uri)
+	    		));
+
+	    	return $app['twig']->render('disqus_as_tos.twig', array('accept_url'=>$accept_url));
+	    })->bind('disqus_as_callback_tos');
 
         return $disqus;
     }
